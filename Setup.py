f72353c9d416f444f1cadfcd8db0c37137b84034
edyanez11 <edgar_yc6468@hotmail.com>
2017-07-13 05:31:58 -0500

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from  distutils.core import setup
+      
+
+setup(	
+	name="CurvHor",
+	version="0.1",
+	long_description="Diseñado para la obtención de los elementos que componen una curva horizontal a partir solamente del 'radio' y del 'angulo de deflexión'",
+	license="MIT",
+	packages=["curvhor"])

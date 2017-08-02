ca3b976e8b6e7a6d87b3ff7e8b4430f8ca01e8a3
edyanez11 <edgar_yc6468@hotmail.com>
2017-08-02 07:56:38 -0500

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -5,6 +5,6 @@ from  distutils.core import setup
 setup(	
 	name="CurvHor",
 	version="0.1",
-	long_description="Diseñado para la obtención de los elementos que componen una curva horizontal a partir solamente del 'radio' y del 'angulo de deflexión'",
+	long_description="Diseñado para la obtención de los elementos que componen una curva horizontal a partir de un archivo CSV y del 'angulo de deflexión'",
 	license="MIT",
 	packages=["curvhor"])
